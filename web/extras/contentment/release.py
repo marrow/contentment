f9caa17e6f17a3ebcb24de16c35fec1f4b5350ae
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-09-28 16:49:50 -0700

    Skeleton files.

---
+++ web/extras/contentment/release.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+"""Release information about Contentment."""
+
+
+name = "contentment"
+version = "0.0.1"
+release = "0.0.1"
+
+summary = "Powerful and extensible content management framework for WebCore."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice+contentment@gothcandy.com"
+url = "http://github.com/GothAlice/Contentment"
+download_url = "http://cheeseshop.python.org/pypi/contentment/"
+copyright = "2010, Alice Bevan-McGregor"
+license = "MIT"
