3ec8bc273b4e0e5a1c271fe9e0d4198b1b52353d
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-05-17 13:55:38 -0700

    Fix typo.

--- web/extras/contentment/components/alert/model.py
+++ web/extras/contentment/components/alert/model.py
@@ -21,7 +21,7 @@ class Alert(Page):
     priority = db.StringField(default="information", choices=['check', 'information', 'question', 'warning'], max_length=11)
     
     def embed(self):
-        return tag.table ( class_ = "alert-embed alert-" + self.priority, onclick="window.location = '" + self.path "';" ) [
+        return tag.table ( class_ = "alert-embed alert-" + self.priority, onclick="window.location = '" + self.path + "';" ) [
                 tag.tr [
                         ([ tag.td ( class_ = "alert-image", rowspan = 2 ) [ tag.Text(self.children[0].embed(), escape=False) ] ] if self.children and hasattr(self.children[0], 'embed') else [ tag.td ( class_ = "alert-image", rowspan = 2 ) [ tag.Text('&nbsp;', escape=False) ] ]) + [
                         tag.th [ self.title ],
