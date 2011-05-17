1a81ab7d866cb2fa69f5e29475060b845199b12b
Alice Bevan-McGregor <alice.mcgregor@me.com>
2011-05-17 13:23:23 -0700

    Switched from using <a href> to using onclick due to browser weirdness attempting to surround a table in a link.

--- web/extras/contentment/components/alert/model.py
+++ web/extras/contentment/components/alert/model.py
@@ -21,7 +21,7 @@ class Alert(Page):
     priority = db.StringField(default="information", choices=['check', 'information', 'question', 'warning'], max_length=11)
     
     def embed(self):
-        return tag.a ( href = self.path ) [ tag.table ( class_ = "alert-embed alert-" + self.priority ) [
+        return tag.table ( class_ = "alert-embed alert-" + self.priority, onclick="window.location = '" + self.path "';" ) [
                 tag.tr [
                         ([ tag.td ( class_ = "alert-image", rowspan = 2 ) [ tag.Text(self.children[0].embed(), escape=False) ] ] if self.children and hasattr(self.children[0], 'embed') else [ tag.td ( class_ = "alert-image", rowspan = 2 ) [ tag.Text('&nbsp;', escape=False) ] ]) + [
                         tag.th [ self.title ],
@@ -29,4 +29,4 @@ class Alert(Page):
                 tag.tr [
                         tag.td [ self.description ]
                     ]
-            ]]
+            ]
