b69c3dfe7f8209e259329166e3c86633046e000d
Alice Bevan–McGregor <alice@gothcandy.com>
2016-01-10 01:24:39 -0500

    Domain mapping and root/theme loading.

--- web/ext/contentment.py
+++ web/ext/contentment.py
@@ -1,10 +1,30 @@
 # encoding: utf-8
 
+from marrow.package.loader import load
 from marrow.package.host import PluginManager
+
+from web.component.asset.model import Asset
+
+
+log = __import__('logging').getLogger(__name__)
 
 
 def indent(context, lines, padding='\t'):
 	return padding + ('\n' + padding).join(lines.split('\n'))
+
+
+MAP = {
+		'localhost': ('career.nse-automatech.com', 'en'),
+		
+		# NSE Automatech
+		# Testing URLs
+		'en.nse.illico.cleverapps.io': ('career.nse-automatech.com', 'en'),
+		'fr.nse.illico.cleverapps.io': ('career.nse-automatech.com', 'fr'),
+		# Production URLs
+		'career.nse-automatech.com': ('career.nse-automatech.com', 'en'),
+		'carrieres.nse-automatech.com': ('career.nse-automatech.com', 'fr'),
+		
+	}
 
 
 class ContentmentExtension:
@@ -35,4 +55,15 @@ class ContentmentExtension:
 		# registry.register(render_asset, Asset)
 	
 	def prepare(self, context):
-		context.domain, _, _ = context.request.host.partition(':')
+		dom = context.request.host.partition(':')[0]
+		parts = MAP.get(dom, (dom, 'en'))
+		context.domain = parts[0]
+		context.lang = parts[1]
+		context.croot = Asset.objects.nearest('/' + context.domain)
+		
+		if context.croot:
+			context.theme = load(context.croot.properties.theme + ':page')
+		else:
+			context.theme = load('web.theme.bootstrap.base:page')
+		
+		log.info("Prepared context.", extra=dict(domain=[dom, context.domain], lang=context.lang, root=repr(context.croot), theme=repr(context.theme)))
