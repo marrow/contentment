dc2aec81831cf4fa394d05deac5345276ed84117
Alice Bevan-McGregor <alice@gothcandy.com>
2016-08-16 08:49:59 -0400

    Simplification of concerns.

--- web/ext/contentment.py
+++ web/ext/contentment.py
@@ -1,78 +1,65 @@
 # encoding: utf-8
 
 from collections import namedtuple
-from functools import partial
 from socket import gethostname
 from marrow.package.loader import load
+from marrow.package.canonical import name
 from marrow.package.host import PluginManager
-from mongoengine import ImageGridFsProxy
-from web.contentment.util import D_
 
-from web.component.asset.model import Asset
+from web.core.context import Context
 
 
 log = __import__('logging').getLogger(__name__)
 
 
-def indent(context, lines, padding='\t'):
-	return padding + ('\n' + padding).join(lines.split('\n'))
+# If the language is None, it will be inferred normally.
+DomainMapping = namedtuple('DomainMapping', ('domain', 'language'))
 
-
-DomainMapping = namedtuple('DomainMapping', ('domain', 'language', 'homepage'))
-
-MAPPING = {'localhost': DomainMapping('localhost', None, 'http://localhost:8080/')}
+MAPPING = {'localhost': DomainMapping('localhost', None)}
 try:
 	_name = gethostname()
-	MAPPING[_name] = DomainMapping(_name, None, None)
+	MAPPING[_name] = DomainMapping(_name, None)
+except:
+	pass
 
 
 class ContentmentExtension:
 	"""WebCore extension for managing Contentment concerns.
 	
-	On startup this registers and enumerates the various Contentment plugin namespaces. It provides a minimal number
-	of configuration options:
-	
-	* `prefixed` - Use the server_name as a path prefix when looking up assets matching the current URI?
+	On startup this registers and enumerates the various Contentment plugin namespaces.
 	"""
 	
-	needs = {'mongodb'}
+	needs = {'mongodb', 'serialization'}
+	provides = {'contentment'}
+	extensions = {'web.component'}
 	
-	def __init__(self, prefixed=False):
-		self.prefixed = prefixed
+	def __init__(self):
+		self.context = Context(mapping=dict(MAPPING))
 	
 	def start(self, context):
 		log.info("Starting Contentment extension.")
+		c9t = context.contentment = self.context.promote('ContentmentContext', False)
 		
-		for asset_type in PluginManager('web.component'):
-			log.info("Found asset type: " + repr(asset_type))
-		
-		context.view.register(dict, self.render_json_response)
+		for namespace in self.extensions:
+			if __debug__:
+				log.debug("Preparing Contentment plugin namespace: " + namespace)
+			
+			setattr(c9t, namespace.rpartition('.')[2], PluginManager(namespace))
 	
 	def prepare(self, context):
-		dom = context.request.host.partition(':')[0]
-		parts = MAP.get(dom, (dom, 'en', ''))
-		context.domain = parts[0]
-		context.lang = parts[1]
-		context.otherlang = parts[2]
-		context.site = context.croot = Asset.objects.nearest('/' + context.domain)
-		context.D = partial(D_, lang=parts[1])
-		context.replacements = dict(context=context)
+		req = context.request
+		c9t = context.contentment = context.contentment()  # Instantiate the ContentmentContext for this request.
 		
-		if context.croot:
-			context.theme = load(context.croot.properties.theme + ':page')
+		c9t.domain = c9t.domain_mapping.get(req.server_name, DomainMapping(req.server_name, None))
+		c9t.site = c9t.taxonomy.nearest('/' + c9t.domain)  # TODO: Defer or cache this?
+		c9t.replacements = dict(context=context)
+		
+		# We allow for colocation of sites, so we need to pull this out of the active site on demand.  TODO: Defer?
+		if c9t.site and 'theme' in c9t.site.properties:
+			c9t.theme = load(c9t.site.properties.theme + ':page')
 		else:
-			context.theme = load('web.theme.bootstrap.base:page')
+			c9t.theme = load('web.theme.bootstrap.base:page')
 		
-		log.info("Prepared context.", extra=dict(domain=[dom, context.domain], lang=context.lang, root=repr(context.croot), theme=repr(context.theme)))
-	
-	def render_json_response(self, context, result):
-		import json
-		
-		response = context.response
-		
-		response.content_type = 'application/json'
-		response.encoding = 'utf-8'
-		response.text = json.dumps(result)
-
-		return True
+		if __debug__:
+			log.debug("Prepared Contentment context.", extra=dict(domain=c9t.domain, site=repr(c9t.site), theme=name(context.theme)))
 
