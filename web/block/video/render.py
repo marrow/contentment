0345b6126c49cddf3eaab5b99a465a5ffe8b9ee1
Alice Bevan-McGregor <alice@gothcandy.com>
2017-05-30 02:18:50 -0400

    EOL.

--- web/block/video/render.py
+++ web/block/video/render.py
@@ -15,4 +15,3 @@
 <div&{id=block.properties.get('id', block.id), class_=classes}>
 	<iframe width="560" height="315" src="https://www.youtube.com/embed/${block.video}" frameborder="0" allowfullscreen></iframe>
 </div>
-
